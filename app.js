c50ce0180c2b4b3717cf575c497b616884230227
xxxx <142011326+xxx-hx@users.noreply.github.com>
2025-07-24 00:49:44 +0700

    app.js

--- app.js
+++ app.js
@@ -111,12 +111,10 @@ async function revokeToken(refreshToken) {
 }
 
 module.exports = async (req, res) => {
-    // ⭐️ Allow CORS for all origins
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-    // Handle preflight
     if (req.method === 'OPTIONS') {
         return res.status(204).end();
     }
